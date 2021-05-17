20e4f8df49f9592b6007d2a9d993413428b283e2
ezzum <lihach.ac@gmail.com>
2021-05-17 23:41:42 +0400

    header template, style

--- src/app/header/header.tsx
+++ src/app/header/header.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
+import './header.scss'
+
 const Header: React.FC = () => {
   return (
     <header className="app__header">
-      <h1 className="app__title">Weather Forecast</h1>
+      <h1 className="app__title title">
+        <span className="title__weather">Weather</span>
+        <span className="title__forecast">forecast</span>
+      </h1>
     </header>
   )
 }
